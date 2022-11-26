3dbda3aef29673e0b6308280d4b9e9a0fd280095
cesar daniel rodriguez <cesar.rodriguezg@usantotomas.edu.co>
2022-11-25 23:48:20 -0500

    se agregan vistas y funcionalidad de botones de la pagina web

---
+++ Aplicacion_Centros_Comerciales/Controllers/Centroscomerciales.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aplicacion_Centros_Comerciales.Controllers
+{
+    public class Centroscomerciales : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CentrocomercialView(string change)
+        {
+            if (change == "1")
+            {
+                return View("CentrocomercialPortal80");
+            }
+            if (change=="2")
+            {
+                return View("CentrocomercialAndino");
+            }
+            if(change=="3")
+            {
+                return View("CentrocomercialParquelacolina");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Aplicacion_Centros_Comerciales/Controllers/LoginController1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aplicacion_Centros_Comerciales.Controllers
+{
+    public class LoginController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult LoginView(string Correo, string contraseña)
+        {
+            bool TipoUsuario = false;
+
+            return View();
+        }
+
+    }
+}
